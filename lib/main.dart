08465cd718de1852a441283e1a8f2743d02c7c97
astutz96 <astutz96.development@protonmail.com>
2020-10-22 17:39:27 -0400

    Added shopping list

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'dart:developer';
 
 void main() {
   runApp(MaterialApp(
     title: 'Shopping App',
-    home: Text('Shopping Cart'),
+    home: ShoppingList(
+      products: <Product>[
+        Product(name: 'Eggs'),
+        Product(name: 'Flour'),
+        Product(name: 'Chocolate chips'),
+      ],
+    ),
   ));
 }
 
@@ -52,3 +57,48 @@ class ShoppingListItem extends StatelessWidget {
   }
 }
 
+class ShoppingList extends StatefulWidget{
+
+  final List<Product> products;
+  //For more info on Keys see https://api.flutter.dev/flutter/foundation/Key-class.html
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  //subclasses of State are typically named with leading underscores to indicate that they are private implementation details
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+// When this widget’s parent rebuilds, the parent creates a new instance of ShoppingList,
+// but the framework reuses the _ShoppingListState instance that is already in the tree rather than calling createState again.
+class _ShoppingListState extends State<ShoppingList> {
+
+  Set<Product> _shoppingCart = Set<Product>();
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (!inCart)
+        _shoppingCart.add(product);
+      else
+        _shoppingCart.remove(product);
+    });
+  }
+  //you don’t need to write separate code for creating and updating child widgets.
+  //Instead, you simply implement the build function, which handles both situations.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ShoppingList'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
